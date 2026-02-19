29fedc12f32435d98dc53c1c5a2af8fc7ac1d38b
518luck <165388484+518luck@users.noreply.github.com>
2026-02-19 17:42:22 +0800

    feat: 添加微服务通信支持
    - 在 exam 服务中配置 TCP 微服务并添加 sum 消息处理器
    - 在 answer 服务中注册微服务客户端并调用 exam 服务的 sum 方法
    - 添加 @nestjs/microservices 依赖和各个服务的启动脚本

--- apps/answer/src/answer.controller.ts
+++ apps/answer/src/answer.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AnswerService } from './answer.service';
+import { ClientProxy } from '@nestjs/microservices';
+import { firstValueFrom } from 'rxjs';
 
 @Controller()
 export class AnswerController {
   constructor(private readonly answerService: AnswerService) {}
 
+  @Inject('EXAM_SERVICE')
+  private examClient: ClientProxy;
+
   @Get()
-  getHello(): string {
-    return this.answerService.getHello();
+  async getHello() {
+    //send():请求/响应模式。你会等待微服务算完并给你回传结果。
+    //emit():事件驱动模式。只负责发送，不关心结果，也不等回复。
+    const value = await firstValueFrom<number>(
+      this.examClient.send('sum', [1, 3, 5]),
+    );
+    return this.answerService.getHello() + ' ' + value;
   }
 }
--- apps/exam/src/main.ts
+++ apps/exam/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { ExamModule } from './exam.module';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(ExamModule);
+
+  // 配置微服务
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: {
+      port: 8888,
+    },
+  });
+
+  // 启动微服务
+  await app.startAllMicroservices();
+
   await app.listen(3002);
 }
 void bootstrap();
